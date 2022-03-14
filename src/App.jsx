25da94b410641c792e2ebf5c0c9bc63f4182db4b
kushagra kamal <kushagrakamal98@gmail.com>
2022-03-14 11:21:00 +0530

    first ct

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,19 @@
 import "./App.css";
 import { Rentals } from "./Components/Rentals/Rentals";
 import { AddHouse } from "./Components/AddHouse/AddHouse";
-import {useState} from "react"
 
 function App() {
-const [tt,settt]= useState(true);
-
   return (
     <div className="App">
-
-      return(
-      <button className="toggleForm" onclick={()=>tt?settt(false):settt(true)}>
-     {tt?<Rentals/>:<AddHouse/>} 
-
-     fkeoijsubb
+      <button className="toggleForm">
+      {<AddHouse/>}
       </button>
-     {tt?<Rentals/>:<AddHouse/>}
+      <button>
+      {Rentals}
+      </button>
      
       <br />
     </div>
-  
   );
 }
 
